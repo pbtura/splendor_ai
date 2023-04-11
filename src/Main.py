f33cbb7614fe30a1f5410b6cbc439cfd6c880f9d
patrick buchheit <bucpatr1@gmail.com>
2023-04-11 14:44:05 -0400

    added a token store class

--- src/Main.py
+++ src/Main.py
@@ -8,13 +8,16 @@ from ResourceCard import ResourceCard
 from Color import Color
 from CardCost import CardCost
 from NobleCard import NobleCard
+from TokenStore import TokenStore
 
 if __name__ == "__main__":
     foo = GemCost(0,1,2,3,4)
     nb = CardCost(4,3,2,1,0)
     card = ResourceCard(1, Color.RED, foo, 2)
     noble = NobleCard(nb)
+    store = TokenStore(7,6,5,4,3,2)
    
     print ("Hello, world!")
     print("resource card is", card)
     print("noble card is", noble)
+    print("token store is", store)
